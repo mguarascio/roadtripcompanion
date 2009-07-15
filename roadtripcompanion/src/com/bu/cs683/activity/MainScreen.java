67bb52370d3b377741296ccefa7071d331cd2ba3
mjurkoic <mjurkoic@16d8adf4-610a-11de-8832-3f4c773c73ae>
2009-07-15 19:46:08 +0000

    This is the initial MainScreen Activity class

---
+++ roadtripcompanion/src/com/bu/cs683/activity/MainScreen.java
@@ -0,0 +1,14 @@
+package com.bu.cs683.activity;
+
+import android.app.Activity;
+
+public class MainScreen extends Activity
+{
+
+	private static final String TAG = "MainScreen";
+	
+	public MainScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
