8a4332218fb960e2cf18becba449c6ba22693fc9
mjurkoic <mjurkoic@16d8adf4-610a-11de-8832-3f4c773c73ae>
2009-07-17 03:03:58 +0000

    Modified the Twitter button to respond to a click. A TextView is temporarily loaded with the text string "SAUSAGE".

--- trunk/roadtripcompanion/src/com/bu/cs683/activity/MainScreen.java
+++ trunk/roadtripcompanion/src/com/bu/cs683/activity/MainScreen.java
@@ -1,14 +1,52 @@
 package com.bu.cs683.activity;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import com.bu.cs683.persistence.RoadTripCompanion;
+import com.bu.cs683.persistence.RoadTripCompanion.Trip;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainScreen extends Activity
 {
-
+	ImageButton mTweet;
+	TextView mText;
+	
+	static final int DATE_DIALOG_ID = 0;
 	private static final String TAG = "MainScreen";
 	
-	public MainScreen() {
-		// TODO Auto-generated constructor stub
+	/** Called when the activity is first created. */
+	//savedInstanceState
+	@Override
+	public void onCreate(Bundle icicle)
+	{
+		super.onCreate(icicle);
+		Log.v(TAG, "#### onCreate()");
+		setContentView(R.layout.mainscreen);
+		
+		
+		mTweet = (ImageButton) findViewById(R.id.tweetButton);
+		mText = (TextView)findViewById(R.id.TextView01);
+		
+		mTweet.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				//showDialog(2);
+				mText.setText("SAUSAGE");
+			}
+		});
 	}
-
 }
+
