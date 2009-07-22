044712e8ab2c2487f0080e2642a0d67a57c33ed3
mjurkoic <mjurkoic@16d8adf4-610a-11de-8832-3f4c773c73ae>
2009-07-22 00:34:18 +0000

    MAJ Working on displaying the TwitterLogin activity from the Tweet button on the MainScreen activity

--- trunk/roadtripcompanion/src/com/bu/cs683/activity/TweetLogin.java
+++ trunk/roadtripcompanion/src/com/bu/cs683/activity/TweetLogin.java
@@ -1,7 +1,7 @@
 package com.bu.cs683.activity;
-//package org.apache.twitter;
 
-import java.util.Calendar;
+import android.os.Bundle;
+import android.util.Log;
 import java.util.GregorianCalendar;
 
 import com.bu.cs683.persistence.RoadTripCompanion;
@@ -18,6 +18,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.webkit.WebView;
 import android.widget.EditText;
@@ -25,27 +26,36 @@ import android.widget.EditText;
 //import android.net.http.RequestQueue;
 import android.net.*;
 
-
-
-public class Tweet extends Activity
+public class TweetLogin extends Activity 
 {
-//	static final int GET_LOGIN_INFORMATION = 1;
-	private static final String TAG = "Tweet";
-
-//    WebView webView;
-//    //RequestQueue requestQueue;
-//    String authInfo;
-
+	private ImageButton mCancelButton;
+	private ImageButton mLoginButton;
+		
+	private static final String TAG = "TweetLogin";
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		Log.v(TAG, "#### onCreate()");
-		setContentView(R.layout.tweet);
+		setContentView(R.layout.login);
+		
+		mLoginButton = (ImageButton) findViewById(R.id.loginbtn);
+		mCancelButton = (ImageButton) findViewById(R.id.cancel);
+		
+		Log.d("ABC", "Is null = " + (null == mLoginButton));
+		
+		// Add a click listener for the Twitter Login button
+		mLoginButton.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View v)
+			{
+				// Create an intent to open the Tweet activity
+				//Intent tweetIntent = new Intent(getApplicationContext(), TweetLogin.class);
+				//startActivity(tweetIntent);
+				//mCancelButton.setText("Cancel Clicked");
+			}
+		});
 	}
 }
-	
-
-
-
